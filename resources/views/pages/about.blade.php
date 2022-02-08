01ccddd03eb73e48bcf27cdff2a28b06824be35b
António Miguel <albino77miguel@gmail.com>
2022-02-07 23:36:58 -0800

    changed about information

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.app')
 @section('title','sobre')
 @section('content')
-    
+    <section class="mt-4 container col-md-6">
+        <h4 class="text text-center">António Albino José Miguel</h4>
+            <p>
+                Estudante de Engenharia Informática,<span class="text text-danger"> Desenvolvedor Laravel, Vue.JS, BootStrap.</span>
+                Engressou na Universidade de Belas em 2018, actualmente finalista.
+            </p>
+    </section>
 @endsection
